75562dd7a96881adbf27fe0eca30708d710c8416
Ilias Apostolou <ilias.apostolou.zero@gmail.com>
2021-01-14 00:08:37 +0200

    Fix

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def Request():
     r = requests.get('http://127.0.0.1:8888/api/v0')
     #r = requests.get('https://services9.arcgis.com/N9p5hsImWXAccRNI/arcgis/rest/services/Nc2JKvYFoAEOFCG5JSI6/FeatureServer/2/query?f=json&where=OBJECTID%3D24&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Deaths%20desc&outSR=102100&resultOffset=0&resultRecordCount=200&resultType=standard&cacheHint=true')
     #print(r.text)
-    data_warbanner = json.loads(r.text + "sdf")
+    data_warbanner = json.loads(r.text)
     data_warbanner_url = data_warbanner["1"]["server"]["homeUrl"]
     print(data_warbanner_url)
 
